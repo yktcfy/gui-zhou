7cecfa8c778d7857a5002600a0e251ecec40e9af
yktcfy <zjyktcfy@qq.com>
2025-06-12 15:33:14 +0800

    Update CaijiGuizhouip.sh

--- CaijiGuizhouip.sh
+++ CaijiGuizhouip.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "开始测试······"
   echo "在5秒内输入1~4可选择城市"
   echo "请选择城市："
-  echo "1. 广西（Guangxi_dianxin）"
+  echo "1. 贵州（Guizhou_dianxin）"
   read -t 5 -p "超时未输入,将按默认设置测试" city_choice
 
   if [ -z "$city_choice" ]; then
@@ -18,7 +18,7 @@ fi
 case $city_choice in
     1)
         city="Guangxi_dianxin"
-        stream="rtp/239.81.0.250:4056"
+        stream="rtp/238.255.2.2:5999"
         ;;
     0)
         # 逐个处理{ }内每个选项
